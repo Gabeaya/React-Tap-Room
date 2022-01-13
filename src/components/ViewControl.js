e8bd9c2d7f7db6cb01ab51d1e4e8605a939660fd
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-13 12:59:27 -0800

    Add a button for the form.

--- src/components/ViewControl.js
+++ src/components/ViewControl.js
@@ -12,14 +12,17 @@ class ViewControl extends React.Component {
   }
   render(){
     let currentlyVisibleState = null;
+    let addTapbutton = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTapForm />
     } else {
       currentlyVisibleState = <Menu />
+      addTapButton = <button onClick={this.handleClick}>Add Tap</button>
     }
     return(
       <React.Fragment>
         {currentlyVisibleState}
+        {addTicketButton}
       </React.Fragment>
     );
     
