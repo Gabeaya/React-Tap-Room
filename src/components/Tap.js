85204f250138f74c6d270e334c2a6cb33601304b
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-13 12:22:56 -0800

    Include extra props.

--- src/components/Tap.js
+++ src/components/Tap.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 function Tap(props){
   const name = "Weihenstephaner Vitus";
   return (
     <React.Fragment>
-    <h2>{ props.name } </h2>
-    <h3>{ props.brand } </h3>
-    <h3><em>{props.price} per pint</em></h3>
+    <h2>{ props.name } - Pint {props.price}</h2>
+    <h3><em>{ props.brand }|ABV: {props.content}</em> </h3>
+
 
     <hr/>
   </React.Fragment>
   );
+}
+
+Tap.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.string,
+  content: PropTypes.string,
+  kegVal: PropTypes.number
 }
 export default Tap;
