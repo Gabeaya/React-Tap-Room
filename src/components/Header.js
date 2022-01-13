c3ac258d3c753a302eb1b9795f23f1b243d434c7
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-13 12:36:49 -0800

    Add an image to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
+import image from "./../img/rick.jpeg";
 
 function Header (){
   return(
-  <h1> Welcome to Ricktoberfest </h1>
+    <React.Fragment>
+      <h1> Welcome to Ricktoberfest </h1>
+      <img src={image} alt="Ricktoberfest logo art" />
+    </React.Fragment>
+  
   );
 }
 
