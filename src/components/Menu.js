a646e4bfcfe99080e2ea56a73e6f6f2308ca3e12
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-13 12:30:09 -0800

    Add hard code drafts.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,34 @@
 import React from "react";
 import Tap from "./Tap";
 
+const mainMenuList = [ 
+  {
+    name: 'Riggity',
+    brand: 'doof',
+    price: '$6',
+    content: '8%',
+    kegVal: 124,
+  },
+  {
+    name: 'Wrecked',
+    brand: 'doof',
+    price: '$7',
+    content: '11%',
+    kegVal: 124,
+  },
+  {
+    name: 'Do the Trick',
+    brand: 'doof',
+    price: '$5',
+    content: '7%',
+    kegVal: 124,
+  }
+]
+
 function Menu(){
+
   return (
-    <Tap 
-      name="Space Dust IPA"
-      brand="hazy"
-      price="$5" 
-      content="5%" 
-      kegVal="124"g />
+   <tap />
   );
 }
 export default Menu;
