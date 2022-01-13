2f0b0753b7866b9a10730dffe6e4cd7f92f13543
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-13 13:37:08 -0800

    Add a form and import uuid.

--- src/components/NewTapForm.js
+++ src/components/NewTapForm.js
@@ -1,10 +1,35 @@
 import React from "react";
-import react from "react";
+
+import { v4 } from 'uuid';
 
 function NewTapForm(props){
   return(
     <React.Fragment>
-      <h3> This is a form</h3>
+      <form onSubmit={handleNewTapFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name of the beverage.' />
+        
+        <input 
+          type='text'
+          name='brand'
+          placeholder='Where is this beverage from?'/>
+        <input 
+          type='text'
+          name='price'
+          placeholder='Price per pint.'/>
+        <input
+          type='text'
+          name='content'
+          placeholder='What is the ABV?'/>
+        <input
+          type='number'
+          name='kegVal'
+          placeholder='Input 124 if its a keg.'/>
+        <button type='submit'>Add to Menu</button>
+      </form>
     </React.Fragment>
   )
 }
+export default NewTapForm;
