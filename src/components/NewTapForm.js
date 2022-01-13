0e0e08f489f5cf3c88ec808bfcff33f9a8529452
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-13 12:45:51 -0800

    Add a form placeholder.

---
+++ src/components/NewTapForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import react from "react";
+
+function NewTapForm(props){
+  return(
+    <React.Fragment>
+      <h3> This is a form</h3>
+    </React.Fragment>
+  )
+}
