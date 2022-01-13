de37fede0054ac090338ce2036701a4a7733eccb
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-13 11:53:11 -0800

    Create the first tap and pass it up to the app.

---
+++ src/components/Tap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Tap(){
+  const name = "Weihenstephaner Vitus";
+  return (
+    <React.Fragment>
+    <h3>{ name } </h3>
+    <hr/>
+  </React.Fragment>
+  );
+}
+export default Tap;
