5b248f5a98764e20c268e78045c96adc86c8ac7e
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-13 12:51:51 -0800

    Add a control to handle the toggling of views.

---
+++ src/components/ViewControl.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NewTapForm from './NewTapForm';  
+import Menu from './Menu';
+
+class ViewControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render(){
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+    
+  }
+}
+export default ViewControl;
