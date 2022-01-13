a7fd5c0e7b48dcbcad4468f83835c36463b32490
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-13 12:55:12 -0800

    who cares.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
-import './App.css';
 import React from 'react';
 import Header from './Header';
-import Menu from './Menu';
+import ViewControl from './ViewControl';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
-      <Menu />
-    </React.Fragment>
+      <ViewControl />
+      </React.Fragment>
     
   );
 }
