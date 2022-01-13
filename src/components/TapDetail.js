f4dfded90c68c634ce182787c895fdd7448b1dff
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-13 14:43:09 -0800

    Add placewholder code.

---
+++ src/components/TapDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TapDetail(prop){
+  return (
+    <React.Fragment>
+      <h1> ticket detail</h1>
+      <hr/>
+    </React.Fragment>
+
+  );
+}
+export default TapDetail;
