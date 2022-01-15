7fea2a334a717aa9c62dfa87f78434699dc422c6
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-15 13:24:22 -0800

    Add event handlers for the edit form.

--- src/components/EditTapForm.js
+++ src/components/EditTapForm.js
@@ -3,9 +3,15 @@ import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
 
 function EditTapForm (props) {
+  const {tap} = props;
+  function  handleEditTapFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTicket({name: event.target.name.value, location: event.target.location.value, price: event.target.price.value, content: event.target.content.value, kegVal: 124, id: ticket.id});
+  }
   return (
     <React.Fragment>
       <ReusableForm 
+        formSubmissionHandler={{handleEditTapFormSubmission}}
         buttonText="Update Tap" />
     </React.Fragment>
   );
