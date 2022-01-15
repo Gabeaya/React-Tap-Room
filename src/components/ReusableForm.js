a4b3d3007bfcc6b615a4005fa093a6ca6b67ab1a
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-15 13:02:11 -0800

    Add a reusable form.

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={handleNewTapFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name of the beverage.' />
+        
+        <input 
+          type='text'
+          name='location'
+          placeholder='Where is this beverage from?'/>
+        <input 
+          type='text'
+          name='price'
+          placeholder='Price per pint.'/>
+        <input
+          type='text'
+          name='content'
+          placeholder='What is the ABV?'/>
+
+        <button type='submit'>Add to Menu</button>
+        <p>*Each submitted form has an automated 124 pints added per Tap item</p>
+
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
