387bb4bc28bd365ecc310da80f71c1e5f8b8b999
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-15 12:27:12 -0800

    add a ticket detail with pints left.

--- src/components/TapDetail.js
+++ src/components/TapDetail.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 function TapDetail(prop){
+  const { tap } = props;
+
   return (
     <React.Fragment>
-      <h1> ticket detail</h1>
+      <h1>Tap Details</h1>
+      <h2>{tap.name}</h2>
+      <h3>Located in {tap.location}</h3>
+      <h3>$({props.price}) a pint</h3>
+      <h3>{props.content}% ABV</h3>
+      <h3>How many pints left:{props.kegVal}</h3>
+
       <hr/>
     </React.Fragment>
 
