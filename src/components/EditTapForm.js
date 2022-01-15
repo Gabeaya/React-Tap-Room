51d0b467085104ab68c349bd22e65fe18bcb9c95
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-15 13:20:53 -0800

    Add the proptypes.

--- src/components/EditTapForm.js
+++ src/components/EditTapForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
 
 function EditTapForm (props) {
   return (
@@ -9,5 +10,8 @@ function EditTapForm (props) {
     </React.Fragment>
   );
 }
-
+EditTapForm.propTypes = {
+  tap: PropTypes.object,
+  onEditTap: PropTypes.func
+}
 export default EditTapForm;
