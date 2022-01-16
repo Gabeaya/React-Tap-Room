ed6b862caa8d9670aebcc70dbd427ea64601f230
Gabriel Ayala <gabeayala100@gmail.com>
2022-01-15 23:40:55 -0800

    Change h status.

--- src/components/TapDetail.js
+++ src/components/TapDetail.js
@@ -6,8 +6,8 @@ function TapDetail(props){
 
   return (
     <React.Fragment>
-      <h1>Tap Details</h1>
-      <h2>{tap.name}</h2>
+      <h2>Tap Details</h2>
+      <h3>{tap.name}</h3>
       <h3>Located in {tap.location}</h3>
       <h3>$ {tap.price} a pint</h3>
       <h3>{tap.content}% ABV</h3>
